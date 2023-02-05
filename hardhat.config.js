301c1b418a518ad9578deeb93edc6672cef2728e
Tebbo <leetebbs@ymail.com>
2023-02-05 13:51:56 +0000

    remove eth from packlok

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,20 +3,20 @@ const fs = require('fs');
 const privateKey = fs.readFileSync(".secret").toString()
 
 module.exports = { 
-  defaultNetwork : "hardhat", 
-  networks: { 
-    hardhat: { 
-      chainId: 1337 
-    }, 
-    wallaby: {  
-      url: `https://wallaby.filfox.info/rpc/v1`,  
-      accounts: [privateKey]  
-    },
-    hyperspace: {  
-      url: `https://filecoin-hyperspace.chainstacklabs.com/rpc/v1`,  
-      accounts: [privateKey]  
-    },
-  }, 
+  // defaultNetwork : "hardhat", 
+  // networks: { 
+  //   hardhat: { 
+  //     chainId: 1337 
+  //   }, 
+  //   wallaby: {  
+  //     url: `https://wallaby.filfox.info/rpc/v1`,  
+  //     accounts: [privateKey]  
+  //   },
+  //   hyperspace: {  
+  //     url: `https://filecoin-hyperspace.chainstacklabs.com/rpc/v1`,  
+  //     accounts: [privateKey]  
+  //   },
+  // }, 
   solidity: { 
     version: "0.8.4", 
   } 
